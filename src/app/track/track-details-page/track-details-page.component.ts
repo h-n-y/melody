d9594cb5da60538d9b4b62b5a8ba153fedcc4531
h-n-y <hnyelek@gmail.com>
2019-01-15 12:15:22 -0700

    Update Track Details page

--- src/app/track/track-details-page/track-details-page.component.ts
+++ src/app/track/track-details-page/track-details-page.component.ts
@@ -9,6 +9,7 @@ import {
 
 // Services
 import { MusicService } from '../../services/music.service';
+import { ModalService } from '../../services/modal.service';
 
 @Component({
     templateUrl: './track-details-page.component.html',
@@ -29,33 +30,53 @@ export class TrackDetailsPageComponent implements OnInit, OnDestroy  {
         this.musicService.fetchTrackWithId(trackId);
     }
 
+    private fetchLyricsForTrackWithId(trackId: number) {
+        this.musicService.fetchLyricsForTrackWithId(trackId);
+    }
+
+    private listenForTrack() {
+        this.musicService.trackForId$.subscribe((track: Track) => {
+            this.track = track;     
+        });
+    }
+
     private listenForTrackLyrics() {
         this.musicService.trackLyricsForId$.subscribe((lyrics: Lyrics) => {
             console.log('lyrics!!!!!');
             console.log(lyrics);
             this.lyrics = lyrics;
             this.lyricsLines = this.lyrics.lyrics_body.split('\n');
-        }):
+        });
+    }
+
+    private listenForModalDismissal() {
+        // Scroll to top of page if modal was just dismissed.
+        this.modalService.announceModalDismissal$.subscribe(() => {
+            document.body.scrollTop = 0;
+        });
     }
 
     constructor(private route: ActivatedRoute,
-               private musicService: MusicService) { }
+               private musicService: MusicService,
+               private modalService: ModalService) { }
 
     ngOnInit() {
+        this.listenForTrack();
         this.listenForTrackLyrics();
 
+        this.listenForModalDismissal();
 
-        this.track = JSON.parse(window.localStorage.getItem('track'));
-        console.log(this.track);
 
-        this.musicService.fetchLyricsForTrackWithId(this.track.track_id);
+        //this.track = JSON.parse(window.localStorage.getItem('track'));
+        //console.log(this.track);
+        //this.musicService.fetchLyricsForTrackWithId(this.track.track_id);
 
-        /*
         this.paramsSub = this.route.params.subscribe((params) => {
+            console.log('--------------------------------------');
             this.trackId = +params['trackId'];
             this.fetchTrack(this.trackId);
+            this.fetchLyricsForTrackWithId(this.trackId);
         });
-       */
     }
 
     ngOnDestroy() {
