ee824655157de1056e9b1fdaa8d6a3387cf15255
h-n-y <hnyelek@gmail.com>
2019-04-10 20:04:04 -0600

    Change application title to 'Melody'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 // Services
 import { ModalService } from './services/modal.service';
+
+const APPLICATION_TITLE = 'Melody';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'music-search';
 
     private dismissModal() {
         this.modalService.destroy();
     }
 
-    constructor(private modalService: ModalService) { }
+    /**
+     * Sets the text node in the <title></title> tag
+     * @param {string} title - The name to assign the title tag.
+     */
+    private setAppTitle(title: string) {
+        this.titleService.setTitle(title);
+    }
+
+    constructor(private modalService: ModalService,
+                private titleService: Title) { }
 
     /**
      * Called after user clicks the search bar in the header.
@@ -34,4 +46,12 @@ export class AppComponent {
         this.dismissModal();
     }
 
+
+    //
+    // LIFECYCLE HOOKS
+    //
+
+    ngOnInit() {
+        this.setAppTitle(APPLICATION_TITLE);
+    }
 }
